b381ce77a0cc19c87f3c97980c77273d8d46941d
Hyejinthedev <Hyejinthedev@gmail.com>
2026-08-21 13:49:17 +0700

    build: compile service worker version update for SRT download feature

--- backend/public/flutter_bootstrap.js
+++ backend/public/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3244406427"
+    serviceWorkerVersion: "2243964794"
   }
 });
